a77a6e6cdfe4c0b6ffab61fe95954b491d2b7f55
Ali Skinner <awskinner26@gmail.com>
2017-05-17 11:08:46 +0100

    added test for Movie component

--- app/tests/components/Movie.test.jsx
+++ app/tests/components/Movie.test.jsx
@@ -1,9 +1,28 @@
 import expect from 'expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import $ from 'jQuery';
+
 import Movie from 'Movie';
+
 
 describe('Movie', () => {
 
   it('should exist', () => {
+    expect(Movie).toExist();
+  });
+
+  // TODO: Improve this test
+  it('should fill Movie component with correct data', () => {
+    let movie = {
+      id: 1,
+      title: 'Jaws',
+      genre: 'Thriller',
+      posterPath: '/some/url.jpg',
+      rating: 10,
+    };
+    let movieComponent = TestUtils.renderIntoDocument(<Movie {...movie}/>);
     expect(Movie).toExist();
   });
 
