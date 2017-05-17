fdb67fd643615375f63e18d79ab307ada6bfcc14
Ali Skinner <awskinner26@gmail.com>
2017-05-17 12:19:25 +0100

    problem with SearchForm test spy

--- app/tests/components/SearchForm.test.jsx
+++ app/tests/components/SearchForm.test.jsx
@@ -1,10 +1,27 @@
 import expect from 'expect';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import $ from 'jQuery';
+
 import SearchForm from 'SearchForm';
+
 
 describe('SearchForm', () => {
 
   it('should exist', () => {
     expect(SearchForm).toExist();
+  });
+
+  // TODO: Fix this test
+  it('should call onFormSubmit on submit', () => {
+    let spy = expect.createSpy();
+    let searchForm = TestUtils.renderIntoDocument(<SearchForm onSubmit={spy} />);
+    let $el = $(ReactDOM.findDOMNode(searchForm));
+
+    searchForm.refs.searchTerm.value = 'jaws';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+    expect(spy).toHaveBeenCalledWith('jaws');
   });
 
 });
